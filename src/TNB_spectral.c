86733eb326585438a5bebacb68f6e72a97516e7b
Jeff Haack <haack@lanl.gov>
2020-08-19 11:05:40 -0600

    Add Bosch and Hale TNB fits

--- src/TNB_spectral.c
+++ src/TNB_spectral.c
@@ -40,11 +40,65 @@ struct TNB_data {
   char name[16];
 };
 
+static struct TNB_data DT;
+static struct TNB_data DDHE;
+static struct TNB_data DDT;
+
 void initializeTNB(int Nv_in, double **c_in, double **wts_in) {
   printf("Initializing TNB\n");
   Nv = Nv_in;
   c = c_in;
   wts = wts_in;
+
+  // Set up data structures for the reactions
+
+  // DT
+  DT.a1 = 6.927e4;
+  DT.a2 = 7.454e8;
+  DT.a3 = 2.050e6;
+  DT.a4 = 5.2002e4;
+  DT.a5 = 0;
+  DT.b1 = 6.38e1;
+  DT.b2 = -9.95e-1;
+  DT.b3 = 6.981e-5;
+  DT.b4 = 1.728e-4;
+  DT.B_G = 34.33827;
+
+  DT.mu_reaction = 2.0053e-24;
+
+  DT.name = "DT";
+
+  // DD-He3
+  DDHE.a1 = 5.3701e4;
+  DDHE.a2 = 3.3027e2;
+  DDHE.a3 = -1.2706e-1;
+  DDHE.a4 = 2.9327e-5;
+  DDHE.a5 = -2.5151e-9;
+  DDHE.b1 = 0.0;
+  DDHE.b2 = 0.0;
+  DDHE.b3 = 0.0;
+  DDHE.b4 = 0.0;
+  DDHE.B_G = 31.3970;
+
+  DDHE.mu_reaction = 1.672e-14;
+
+  DDGE.name = "DD-He3";
+
+  // DD-T
+  DDT.a1 = 5.5576e4;
+  DDT.a2 = 2.1054e2;
+  DDT.a3 = -3.2638e-2;
+  DDT.a4 = 1.4987e-6;
+  DDT.a5 = 1.8181e-10;
+  DDT.b1 = 0.0;
+  DDT.b2 = 0.0;
+  DDT.b3 = 0.0;
+  DDT.b4 = 0.0;
+  DDT.B_G = 31.3970;
+
+  DDT.mu_reaction = 1.672e-14;
+
+  DDT.name = "DD-T";
 }
 
 // forward declaration
@@ -53,9 +107,10 @@ void generate_conv_weights(double *conv_weights,
 
 // Generic TNB calculator
 // This takes input data for the various flavors of TNB reactions
-// Computes Q_TNB(c) = \int |g| \sigma(|g|) f_1(c) f_2(c_\ast) d\c_ast
-void TNB_generic(struct TNB_data *reaction_info, double mu, double *f1,
-                 double *f2, int sp, int sp2, double *Q_TNB) {
+// Not this assumes that you have already checked the species to make sure that
+// it is TNB Computes Q_TNB(c) = \int |g| \sigma(|g|) f_1(c) f_2(c_\ast) d\c_ast
+void TNB_generic(struct TNB_data *reaction_info, double *f1, double *f2, int sp,
+                 int sp2, double *Q_TNB) {
 
   // Initialize Q_TNB
   for (int i = 0; i < Nv * Nv * Nv; i++) {
@@ -64,11 +119,6 @@ void TNB_generic(struct TNB_data *reaction_info, double mu, double *f1,
 
   // initialize weight array
   double *conv_weights = malloc(Nv * Nv * Nv * sizeof(double));
-
-  // check to see if we have the right species setup
-  if ((mu - reaction_info->mu_reaction) / reaction_info->mu_reaction > 0.01) {
-    return;
-  }
 
   // Check to see if the weight is generated
   char weight_filename[256] = "Data/";
@@ -139,3 +189,50 @@ void generate_conv_weights(double *conv_weights,
     }
   }
 }
+
+// Calculates loss function due to DT TNB reactions for all velocity points
+// Q_TNB(c) = \int g sigma_{DT}(g) f(c) f(v_\ast) d \v_ast
+double GetTNB_dt(double mu, double *in, double *c1, int sp, int sp2);
+
+// Calculates the total dt reaction rate in a cell
+// R_DT = \int Q_TNB(c) dc
+double GetReactivity_dt(double mu, double *in, double *in2, int sp, int sp2);
+
+//-----------------------------------------//
+
+// Calculates loss function due to dd->he TNB reactions for all velocity points
+// Q_TNB(c) = \int g sigma_{DDHe}(g) f(c) f(v_\ast) d \v_ast
+double GetTNB_dd_He(double mu, double *in, double *c1, int sp, int sp2);
+
+// Calculates the total dd->he reaction rate in a cell
+// R_DDHe = \int Q_TNB(c) dc
+double GetReactivity_dd_He(double mu, double *in, double *in2, int sp, int sp2);
+
+//-----------------------------------------//
+
+// Calculates loss function due to dd->T TNB reactions for all velocity points
+// Q_TNB(c) = \int g sigma_{DDT}(g) f(c) f(v_\ast) d \v_ast
+double GetTNB_dd_T(double mu, double *in, double *c1, int sp, int sp2);
+
+// Calculates the total dd->T reaction rate in a cell
+// R_DDT = \int Q_TNB(c) dc
+double GetReactivity_dd_T(double mu, double *in, double *in2, int sp, int sp2);
+
+//-----------------------------------------//
+
+// Calculates loss function due to TT TNB reactions for all velocity points
+// Q_TNB(c) = \int g sigma_{TT}(g) f(c) f(v_\ast) d \v_ast
+double GetTNB_tt(double mu, double *in, double *c1, int sp, int sp2);
+
+// Calculates the total TT reaction rate in a cell
+// R_TT = \int Q_TNB(c) dc
+double GetReactivity_tt(double mu, double *in, double *in2, int sp, int sp2);
+
+//-----------------------------------------//
+
+// I think this does the actual tail depletion
+void TNB_DD(double **f, double **f_out, int sp, int rank, int TNB_FLAG,
+            double dt, double mu, double *n, double **v, double *T);
+
+void TNB_DT(double **f, double **f_out, int sp, int sp2, int rank, int TNB_FLAG,
+            double dt, double mu, double *n, double **v, double *T);
